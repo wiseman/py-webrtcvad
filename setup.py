56c3cb303522fe8c30623c2c5e2d0b37de2b8148
John Wiseman <jjwiseman@gmail.com>
2016-04-23 14:36:21 -0700

    Not compatible with Python 2.6.
    _Is_ compatible with 2.7, 3.2, 3.3, 3.4, 3.5.

--- setup.py
+++ setup.py
@@ -19,17 +19,42 @@ c_sources = (
             'vad',
             '*.c')))
 
-
 module = Extension('_webrtcvad',
                    define_macros=[('WEBRTC_POSIX', None)],
                    sources=c_sources,
                    include_dirs=['cbits'])
 
-
 setup(
     name='PackageName',
     version='1.0',
     description='This is a demo package',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     ext_modules=[module],
     py_modules=['webrtcvad'],
     test_suite='nose.collector',
