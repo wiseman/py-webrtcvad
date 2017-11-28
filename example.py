a0f53c7c58f959f8db329c6201a043b9f63a660c
John Wiseman <jjwiseman@gmail.com>
2017-11-28 10:44:59 -0800

    Added comments to example.py. Fixes #10.

--- example.py
+++ example.py
@@ -7,6 +7,10 @@ import webrtcvad
 
 
 def read_wave(path):
+    """Reads a .wav file.
+
+    Takes the path, and returns (PCM audio data, sample rate).
+    """
     with contextlib.closing(wave.open(path, 'rb')) as wf:
         num_channels = wf.getnchannels()
         assert num_channels == 1
@@ -19,6 +23,10 @@ def read_wave(path):
 
 
 def write_wave(path, audio, sample_rate):
+    """Writes a .wav file.
+
+    Takes path, PCM audio data, and sample rate.
+    """
     with contextlib.closing(wave.open(path, 'wb')) as wf:
         wf.setnchannels(1)
         wf.setsampwidth(2)
@@ -27,6 +35,7 @@ def write_wave(path, audio, sample_rate):
 
 
 class Frame(object):
+    """Represents a "frame" of audio data."""
     def __init__(self, bytes, timestamp, duration):
         self.bytes = bytes
         self.timestamp = timestamp
@@ -34,6 +43,13 @@ class Frame(object):
 
 
 def frame_generator(frame_duration_ms, audio, sample_rate):
+    """Generates audio frames from PCM audio data.
+
+    Takes the desired frame duration in milliseconds, the PCM data, and
+    the sample rate.
+
+    Yields Frames of the requested duration.
+    """
     n = int(sample_rate * (frame_duration_ms / 1000.0) * 2)
     offset = 0
     timestamp = 0.0
@@ -46,9 +62,38 @@ def frame_generator(frame_duration_ms, audio, sample_rate):
 
 def vad_collector(sample_rate, frame_duration_ms,
                   padding_duration_ms, vad, frames):
+    """Filters out non-voiced audio frames.
+
+    Given a webrtcvad.Vad and a source of audio frames, yields only
+    the voiced audio.
+
+    Uses a padded, sliding window algorithm over the audio frames.
+    When more than 90% of the frames in the window are voiced (as
+    reported by the VAD), the collector triggers and begins yielding
+    audio frames. Then the collector waits until 90% of the frames in
+    the window are unvoiced to detrigger.
+
+    The window is padded at the front and back to provide a small
+    amount of silence or the beginnings/endings of speech around the
+    voiced frames.
+
+    Arguments:
+
+    sample_rate - The audio sample rate, in Hz.
+    frame_duration_ms - The frame duration in milliseconds.
+    padding_duration_ms - The amount to pad the window, in milliseconds.
+    vad - An instance of webrtcvad.Vad.
+    frames - a source of audio frames (sequence or generator).
+
+    Returns: A generator that yields PCM audio data.
+    """
     num_padding_frames = int(padding_duration_ms / frame_duration_ms)
+    # We use a deque for our sliding window/ring buffer.
     ring_buffer = collections.deque(maxlen=num_padding_frames)
+    # We have two states: TRIGGERED and NOTTRIGGERED. We start in the
+    # TRIGGERED state.
     triggered = False
+
     voiced_frames = []
     for frame in frames:
         sys.stdout.write(
@@ -57,16 +102,27 @@ def vad_collector(sample_rate, frame_duration_ms,
             ring_buffer.append(frame)
             num_voiced = len([f for f in ring_buffer
                               if vad.is_speech(f.bytes, sample_rate)])
+            # If we're NOTTRIGGERED and more than 90% of the frames in
+            # the ring buffer are voiced frames, then enter the
+            # TRIGGERED state.
             if num_voiced > 0.9 * ring_buffer.maxlen:
-                sys.stdout.write('+(%s)' % (ring_buffer[0].timestamp,))
                 triggered = True
+                sys.stdout.write('+(%s)' % (ring_buffer[0].timestamp,))
+                # We want to yield all the audio we see from now until
+                # we are NOTTRIGGERED, but we have to start with the
+                # audio that's already in the ring buffer.
                 voiced_frames.extend(ring_buffer)
                 ring_buffer.clear()
         else:
+            # We're in the TRIGGERED state, so collect the audio data
+            # and add it to the ring buffer.
             voiced_frames.append(frame)
             ring_buffer.append(frame)
             num_unvoiced = len([f for f in ring_buffer
                                 if not vad.is_speech(f.bytes, sample_rate)])
+            # If more than 90% of the frames in the ring buffer are
+            # unvoiced, then enter NOTTRIGGERED and yield whatever
+            # audio we've collected.
             if num_unvoiced > 0.9 * ring_buffer.maxlen:
                 sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
                 triggered = False
@@ -76,6 +132,8 @@ def vad_collector(sample_rate, frame_duration_ms,
     if triggered:
         sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
     sys.stdout.write('\n')
+    # If we have any leftover voiced audio when we run out of input,
+    # yield it.
     if voiced_frames:
         yield b''.join([f.bytes for f in voiced_frames])
 
