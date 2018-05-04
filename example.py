fe9d953217932c319070a6aeeeb6860aeb1c474e
rahimnathwani <rahim@encona.com>
2018-05-04 13:45:40 +0800

    Update example.py to accept 48kHz files

--- example.py
+++ example.py
@@ -17,7 +17,7 @@ def read_wave(path):
         sample_width = wf.getsampwidth()
         assert sample_width == 2
         sample_rate = wf.getframerate()
-        assert sample_rate in (8000, 16000, 32000)
+        assert sample_rate in (8000, 16000, 32000, 48000)
         pcm_data = wf.readframes(wf.getnframes())
         return pcm_data, sample_rate
 
