3bd761332a9404f5c9276105070ee814c4428342
Smaarn <antoine.malliarakis@gmail.com>
2020-10-11 23:26:33 +0200

    added: powerpc platforms support

--- cbits/webrtc/typedefs.h
+++ cbits/webrtc/typedefs.h
@@ -47,6 +47,13 @@
 #elif defined(__pnacl__)
 #define WEBRTC_ARCH_32_BITS
 #define WEBRTC_ARCH_LITTLE_ENDIAN
+#elif defined(__PPC__)
+#if defined(__PPC64__)
+#define WEBRTC_ARCH_64_BITS
+#else
+#define WEBRTC_ARCH_32_BITS
+#endif
+#define WEBRTC_ARCH_BIG_ENDIAN
 #else
 #error Please add support for your architecture in typedefs.h
 #endif
